72f55b64c1af441f41bdf3f2823ad9cf7fb2ba0b
EC2 Default User <ec2-user@ip-172-31-88-132.ec2.internal>
2021-03-25 05:11:13 +0000

    [Finisf]2-4

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,5 +1,18 @@
 class TodolistsController < ApplicationController
   def new
-    @kist = List.new
+    @list = List.new
   end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
+
 end
